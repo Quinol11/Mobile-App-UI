1ce4e41ee00748edeeca2dd20881729c3d206872
KUNAL <34332048+Quinol@users.noreply.github.com>
2019-10-26 03:03:06 +0530

    Design improvements and scene connection established
    > hide Product view
    >screenshot and save function
    >create more categories

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+     public void LoadScene(int level)
+      { 
+         Application.LoadLevel(level);
+       }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

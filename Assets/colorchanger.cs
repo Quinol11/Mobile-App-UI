3018178ad56fb311d67e2907c1322e816c9e1bb7
KUNAL <34332048+Quinol@users.noreply.github.com>
2019-10-27 02:35:16 +0530

    Made AR Scene for sofa1
    >LEFT DRAWER  bugging and interfering with model rotation.
    >Material change script not working

---
+++ Assets/colorchanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorchanger : MonoBehaviour
+{
+    public Material Velvet;
+    public Material Leathery;
+    private Renderer rend; 
+    [SerializeField]
+
+    public void ToWhite(){
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.white;
+    }
+    public void ToRosy(){
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color();
+    }
+
+    public void MakeVelvet(){
+        GetComponent<Renderer>().materials[1]=Velvet;
+    }
+     public void MakeLeather(){
+        GetComponent<Renderer>().materials[1]=Leathery;
+    }
+}
